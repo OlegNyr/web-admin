9e0b4380eba5488e14e749a6066fc28b43fd4688
olegnyr <o.nyrkov@gmail.com>
2020-04-21 12:20:22 +0300

    jib

--- src/main/java/ru/nyrk/admin/AdminApplication.java
+++ src/main/java/ru/nyrk/admin/AdminApplication.java
@@ -4,9 +4,11 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAdminServer
 @EnableDiscoveryClient
+@EnableScheduling
 @SpringBootApplication
 public class AdminApplication {
 
